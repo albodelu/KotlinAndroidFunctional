d696eb3b3ee8dd6072306a32c7b898c254727f8e
Raul Raja <raulraja@gmail.com>
2017-07-15 04:24:03 +0200

    AsyncResult with support for MonadError and Error handling

--- app/src/main/java/com/github/jorgecastillo/kotlinandroid/functional/predef.kt
+++ app/src/main/java/com/github/jorgecastillo/kotlinandroid/functional/predef.kt
@@ -56,30 +56,4 @@ class AsyncResult<A>(val value: EitherT<Future.F, CharacterError, A>) : AsyncRes
                 AsyncResult(ETM.pure(a))
     }
 }
-typealias KleisliF<F, D> = HK2<Kleisli.F, F, D>
-
-class KleisliMonadError<F, D, E>(val FM: MonadError<F, E>) : MonadError<KleisliF<F, D>, E> {
-    override fun <A, B> flatMap(fa: HK<KleisliF<F, D>, A>, f: (A) -> HK<KleisliF<F, D>, B>): Kleisli<F, D, B> =
-            fa.ev().flatMap(f.andThen { it.ev() })
-
-    override fun <A, B> map(fa: HK<KleisliF<F, D>, A>, f: (A) -> B): Kleisli<F, D, B> =
-            fa.ev().map(f)
-
-    override fun <A, B> product(fa: HK<KleisliF<F, D>, A>, fb: HK<KleisliF<F, D>, B>): Kleisli<F, D, Tuple2<A, B>> =
-            Kleisli(FM, { FM.product(fa.ev().run(it), fb.ev().run(it)) })
-
-    override fun <A> handleErrorWith(fa: HK<KleisliF<F, D>, A>, f: (E) -> HK<KleisliF<F, D>, A>): Kleisli<F, D, A> =
-            Kleisli(FM, {
-                FM.handleErrorWith(fa.ev().run(it), { e: E -> f(e).ev().run(it) })
-            })
-
-    override fun <A, B> tailRecM(a: A, f: (A) -> HK<KleisliF<F, D>, Either<A, B>>): Kleisli<F, D, B> =
-            Kleisli(FM, { b -> FM.tailRecM(a, { f(it).ev().run(b) }) })
-
-    override fun <A> raiseError(e: E): Kleisli<F, D, A> =
-            Kleisli(FM, { FM.raiseError(e) })
-
-    override fun <A> pure(a: A): Kleisli<F, D, A> =
-            Kleisli(FM, { FM.pure(a) })
-}
 
