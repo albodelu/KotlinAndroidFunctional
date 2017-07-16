e1be28494a6ea401e15765d429666c8bb397fc58
Raul Raja <raulraja@gmail.com>
2017-07-16 20:37:54 +0200

    Tagless Final style independent of datatypes and with selective runtime

--- app/src/main/java/com/github/jorgecastillo/kotlinandroid/functional/predef.kt
+++ app/src/main/java/com/github/jorgecastillo/kotlinandroid/functional/predef.kt
@@ -4,82 +4,72 @@ import com.github.jorgecastillo.architecturecomponentssample.model.error.*
 import com.github.jorgecastillo.kotlinandroid.di.context.GetHeroesContext
 import kategory.*
 
-typealias Result<A> = Kleisli<AsyncResult.F, GetHeroesContext, A>
+typealias Control<F> = MonadControl<F, GetHeroesContext, CharacterError>
+
+inline fun <reified F> control(): Control<F> = monadControl()
+
 typealias AsyncResultKind<A> = HK<AsyncResult.F, A>
 
 fun <A> AsyncResultKind<A>.ev(): AsyncResult<A> =
         this as AsyncResult<A>
 
+typealias Result<A> = Kleisli<EitherTF<Future.F, CharacterError>, GetHeroesContext, A>
+
 //There should be a monad error instance for EitherT in kategory
-class AsyncResult<A>(val value: EitherT<Future.F, CharacterError, A>) : AsyncResultKind<A> {
-
+class AsyncResult<A>(val value: Result<A>) : AsyncResultKind<A> {
     class F private constructor()
-
-    fun <B> flatMap(f: (A) -> AsyncResultKind<B>): AsyncResult<B> =
-            AsyncResult(ETM.flatMap(value, f.andThen({ it.ev().value })))
-
-    fun <B> map(f: (A) -> B): AsyncResult<B> =
-            AsyncResult(ETM.map(value, f))
-
-    fun <B> product(fb: AsyncResult<B>): AsyncResult<Tuple2<A, B>> =
-            AsyncResult(ETM.product(value, fb.ev().value).ev())
-
-    fun handleErrorWith(f: (CharacterError) -> AsyncResult<A>): AsyncResult<A> {
-        return AsyncResult(EitherT(Future, Future.flatMap(value.value.ev(), {
-            it.fold({ f(it).value.value.ev() }, { Future.pure(Either.Right(it)) })
-        })))
+    companion object :
+            AsyncResultMonadControl,
+            GlobalInstance<MonadControl<AsyncResult.F, GetHeroesContext, CharacterError>>() {
+        fun init() : AsyncResult<Unit> = pure(Unit)
     }
 
-    companion object : MonadError<AsyncResult.F, CharacterError> {
-
-        val ETM = EitherTMonad<Future.F, CharacterError>(Future)
-
-        override fun <A, B> flatMap(fa: AsyncResultKind<A>, f: (A) -> AsyncResultKind<B>): AsyncResult<B> =
-                fa.ev().flatMap(f)
-
-        override fun <A, B> map(fa: AsyncResultKind<A>, f: (A) -> B): HK<F, B> =
-                fa.ev().map(f)
-
-        override fun <A, B> product(fa: AsyncResultKind<A>, fb: AsyncResultKind<B>): AsyncResult<Tuple2<A, B>> =
-                fa.ev().product(fb.ev())
-
-        override fun <A> handleErrorWith(fa: AsyncResultKind<A>, f: (CharacterError) -> AsyncResultKind<A>): AsyncResult<A> =
-                fa.ev().handleErrorWith(f.andThen({ it.ev() }))
-
-        override fun <A, B> tailRecM(a: A, f: (A) -> AsyncResultKind<Either<A, B>>): AsyncResult<B> =
-                AsyncResult(ETM.tailRecM(a, f.andThen({ it.ev().value })))
-
-        override fun <A> raiseError(e: CharacterError): AsyncResult<A> =
-                AsyncResult(EitherT.left(e, Future))
-
-        override fun <A> pure(a: A): AsyncResult<A> =
-                AsyncResult(ETM.pure(a))
-    }
+    fun run(ctx : GetHeroesContext): HK<EitherTF<Future.F, CharacterError>, A> = value.run(ctx)
 }
-typealias KleisliF<F, D> = HK2<Kleisli.F, F, D>
 
-class KleisliMonadError<F, D, E>(val FM: MonadError<F, E>) : MonadError<KleisliF<F, D>, E> {
-    override fun <A, B> flatMap(fa: HK<KleisliF<F, D>, A>, f: (A) -> HK<KleisliF<F, D>, B>): Kleisli<F, D, B> =
-            fa.ev().flatMap(f.andThen { it.ev() })
+interface MonadControl<F, D, E> :
+        MonadError<F, E>,
+        MonadReader<F, D>,
+        Typeclass
 
-    override fun <A, B> map(fa: HK<KleisliF<F, D>, A>, f: (A) -> B): Kleisli<F, D, B> =
-            fa.ev().map(f)
+inline fun <reified F, reified D, reified E> monadControl(): MonadControl<F, D, E> =
+        instance(InstanceParametrizedType(MonadControl::class.java, listOf(F::class.java, D::class.java)))
 
-    override fun <A, B> product(fa: HK<KleisliF<F, D>, A>, fb: HK<KleisliF<F, D>, B>): Kleisli<F, D, Tuple2<A, B>> =
-            Kleisli(FM, { FM.product(fa.ev().run(it), fb.ev().run(it)) })
+interface AsyncResultMonadControl : MonadControl<AsyncResult.F, GetHeroesContext, CharacterError> {
 
-    override fun <A> handleErrorWith(fa: HK<KleisliF<F, D>, A>, f: (E) -> HK<KleisliF<F, D>, A>): Kleisli<F, D, A> =
-            Kleisli(FM, {
-                FM.handleErrorWith(fa.ev().run(it), { e: E -> f(e).ev().run(it) })
-            })
+    fun ETM(): EitherTMonadError<Future.F, CharacterError> =
+            EitherTInstances(Future)
 
-    override fun <A, B> tailRecM(a: A, f: (A) -> HK<KleisliF<F, D>, Either<A, B>>): Kleisli<F, D, B> =
-            Kleisli(FM, { b -> FM.tailRecM(a, { f(it).ev().run(b) }) })
+    fun KM(): KleisliInstances<EitherTF<Future.F, CharacterError>, GetHeroesContext, CharacterError> =
+            KleisliInstances(ETM())
 
-    override fun <A> raiseError(e: E): Kleisli<F, D, A> =
-            Kleisli(FM, { FM.raiseError(e) })
+    override fun <A, B> map(fa: HK<AsyncResult.F, A>, f: (A) -> B): AsyncResult<B> =
+            AsyncResult(KM().map(fa.ev().value, f))
 
-    override fun <A> pure(a: A): Kleisli<F, D, A> =
-            Kleisli(FM, { FM.pure(a) })
+    override fun <A, B> product(fa: HK<AsyncResult.F, A>, fb: HK<AsyncResult.F, B>): AsyncResult<Tuple2<A, B>> =
+            AsyncResult(KM().product(fa.ev().value, fb.ev().value))
+
+    override fun <A, B> flatMap(fa: HK<AsyncResult.F, A>, f: (A) -> HK<AsyncResult.F, B>): AsyncResult<B> =
+            AsyncResult(KM().flatMap(fa.ev().value, f.andThen { it.ev().value }))
+
+    override fun <A> handleErrorWith(fa: HK<AsyncResult.F, A>, f: (CharacterError) -> HK<AsyncResult.F, A>): AsyncResult<A> =
+            AsyncResult(KM().handleErrorWith(fa.ev().value, f.andThen { it.ev().value }))
+
+    override fun <A, B> tailRecM(a: A, f: (A) -> HK<AsyncResult.F, Either<A, B>>): AsyncResult<B> =
+            AsyncResult(KM().tailRecM(a, f.andThen { it.ev().value }))
+
+    override fun <A> raiseError(e: CharacterError): AsyncResult<A> =
+            AsyncResult(KM().raiseError<A>(e))
+
+    override fun <A> pure(a: A): AsyncResult<A> =
+            AsyncResult(KM().pure(a))
+
+    override fun ask(): AsyncResult<GetHeroesContext> =
+            AsyncResult(KM().ask())
+
+    override fun <A> local(f: (GetHeroesContext) -> GetHeroesContext, fa: HK<AsyncResult.F, A>): AsyncResult<A> =
+            AsyncResult(KM().local(f, fa.ev().value))
 }
+
+
 
