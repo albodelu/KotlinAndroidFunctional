4f8dc41fb6cb6e4db29cf6307be4b45dcb6d2d2a
Raul Raja <raulraja@gmail.com>
2017-07-16 20:50:25 +0200

    misc cleanup

--- app/src/main/java/com/github/jorgecastillo/kotlinandroid/KotlinArchitectureApp.kt
+++ app/src/main/java/com/github/jorgecastillo/kotlinandroid/KotlinArchitectureApp.kt
@@ -6,7 +6,6 @@ import com.github.jorgecastillo.kotlinandroid.di.context.GetHeroesContext
 import com.github.jorgecastillo.kotlinandroid.functional.AsyncResult
 import com.github.jorgecastillo.kotlinandroid.functional.MonadControl
 import kategory.GlobalInstances
-import kategory.Id
 import kategory.InstanceParametrizedType
 
 class KotlinArchitectureApp : Application() {
@@ -17,11 +16,10 @@ class KotlinArchitectureApp : Application() {
     }
 
     private fun initGlobalInstances() {
-        val id = Id(1)
         GlobalInstances.putIfAbsent(
                 InstanceParametrizedType(
                         MonadControl::class.java,
-                        listOf(AsyncResult.F::class.java, GetHeroesContext::class.java)
+                        listOf(AsyncResult.F::class.java, GetHeroesContext::class.java, CharacterError::class.java)
                 ),
                 AsyncResult
         )
